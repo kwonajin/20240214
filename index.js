9c2d7ee3933fe947765b04a2f2e7b98e7e6af5b3
Donghwan Oh <imodh97@gmail.com>
2024-02-15 19:17:38 +0900

    complete index.js (odh)

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const Input = require('./userInput');
-let mysql = require('mysql');
+/* Modules */
+const mysql = require("mysql");
+const Input = require("./modules/userInput");
+const runSql = require("./modules/runSql");
+
+/* menu */
+const signUp = require("./menu/signup");
+const login = require("./menu/login");
+const product = require("./menu/product");
+const location = require("./menu/location");
+const bag = require("./menu/bag");
+
+/* wait */
+const wait = (timeToDelay) =>
+  new Promise((resolve) => setTimeout(resolve, timeToDelay));
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -9,3 +22,39 @@ let connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+async function main() {
+  while (true) {
+    console.log(
+      "╔═════════════╦═══════════╦═════════╦═════════╦═════════════╦════════════════╦══════════╗"
+    );
+    console.log(
+      "║ 0. 회원가입 ║ 1. 로그인 ║ 2. 상품 ║ 3. 매장 ║ 4. 장바구니 ║ 5. 마이페이지  ║ 6. 종료  ║"
+    );
+    console.log(
+      "╚═════════════╩═══════════╩═════════╩═════════╩═════════════╩════════════════╩══════════╝"
+    );
+
+    let menu = await Input.getUserInput();
+
+    if (menu === "0") {
+      await signUp.signUp(connection);
+      await wait(3000);
+    } else if (menu === "1") {
+      await login.login(connection);
+      await wait(3000);
+    } else if (menu === "2") {
+      await product.product(connection);
+      await wait(3000);
+    } else if (menu === "3") {
+      await location.location(connection);
+      await wait(3000);
+    } else if (menu === "4") {
+      await bag.main();
+    } else if (menu === "6") {
+      console.log("이용해주셔서 감사합니다.");
+      process.exit();
+    }
+  }
+}
+
+main();
