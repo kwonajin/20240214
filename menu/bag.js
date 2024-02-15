150851ef35f8a5f0465f433949e7cc4fbd147264
seyun00 <reny83@naver.com>
2024-02-15 17:09:41 +0900

    merge files (csy)

--- menu/bag.js
+++ menu/bag.js
@@ -1,5 +1,5 @@
-const Input = require('./userInput');
-let mysql = require('mysql');
+const Input = require('../modules/userInput');
+let mysql = require('../mysql');
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
